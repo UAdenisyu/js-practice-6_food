ad97ef719aa615bea89c8b3fb1387947a738d35b
Denis <denisyu22824@gmail.com>
2021-03-09 19:12:17 +0200

    modal window added

--- js/script.js
+++ js/script.js
@@ -4,6 +4,12 @@ const d = document;
 
 d.addEventListener('DOMContentLoaded', () => {
 
+    //deleting side text panel
+
+    const shit = d.querySelector('.sidepanel');
+    shit.remove();
+
+    //tabs
 
     const tabs = d.querySelectorAll('.tabheader__item'),
           tabsContent = d.querySelectorAll('.tabcontent'),
@@ -40,5 +46,95 @@ d.addEventListener('DOMContentLoaded', () => {
             });
         }
     });
+
+    //timer
+
+    const deadLine = '2021-03-18';
+
+    function getTimeRemaining(endTime){
+        const t = Date.parse(endTime) - Date.parse(new Date()),//miliseconds differense
+              days = Math.floor(t / (1000 * 60 * 60 * 24)),
+              hours = Math.floor(t / (1000 * 60 * 60) % 24),
+              minutes = Math.floor(t / (1000 * 60) % 60),
+              seconds = Math.floor((t / 1000) % 60);
+    return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    };
+    }
+
+    function setZero(num){
+        if (num >= 0 && num < 10){
+            return '0' + num;
+        }
+        else{
+            return num;
+        }
+    }
+
+    function setTimer (selector, time) {
+        const timer = d.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timeInterval = setInterval(updateClock, 1000);
+        
+        updateClock();
+
+        function updateClock() {
+            const t = getTimeRemaining(time);
+
+            days.innerHTML = setZero(t.days);
+            hours.innerHTML = setZero(t.hours);
+            minutes.innerHTML = setZero(t.minutes);
+            seconds.innerHTML = setZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+        
+    }
+
+    setTimer('.timer', deadLine);
+
+    //modal window
+
+    const modalTrigger = d.querySelectorAll('[data-modal]'),
+          modalWindow = d.querySelector('.modal'),
+          modalCloseBtn = modalWindow.querySelector('[data-close]');
+
+
+    modalTrigger.forEach(item => {
+        item.addEventListener('click', (e) => {
+            modalWindow.classList.add('show', 'fade');
+            d.body.style.overflow = 'hidden';
+        });
+    });
+
+
+    function closeModal(){
+        modalWindow.classList.remove('show', 'fade');
+        d.body.style.overflow = 'auto';
+    }
+
+    modalCloseBtn.addEventListener('click', closeModal);
+
+    modalWindow.addEventListener('click', (e) => {
+        if (e.target === modalWindow){
+            closeModal();
+        }
+    });
+
+    d.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape' && modalWindow.classList.contains('show')) {
+            closeModal();
+        }
+    });
+
 });
 
